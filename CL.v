4c1409148e2421e05260cb26b27fff4c8b933006
WorringBus <164392034+WorringBus@users.noreply.github.com>
2025-08-26 20:21:02 -0600

    Actividad 2

---
+++ CL.v
@@ -0,0 +1,8 @@
+module cl_ (input i_bit1, input i_bit2, output o_and, output o_nand, output o_or, output o_nor, output o_xor, output o_xnor);
+assign o_and = i_bit1 & i_bit2;
+assign o_nand = i_bit1 ~& i_bit2;
+assign o_or = i_bit1 | i_bit2;
+assign o_nor = i_bit1 ~| i_bit2;
+assign o_xor = i_bit1 ^ i_bit2;
+assign o_and = i_bit1 ~^ i_bit2;
+endmodule
