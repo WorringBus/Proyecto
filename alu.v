7a5c7f4af969be877fdc46a36b07b242010e9072
WorringBus <164392034+WorringBus@users.noreply.github.com>
2025-09-01 20:02:13 -0600

    Create alu.v

---
+++ alu.v
@@ -0,0 +1,17 @@
+// alu.v
+module ALU (
+    input  [3:0] A,     
+    input  [3:0] B,     
+    input  [1:0] sel,   
+    output reg [4:0] R  
+);
+
+always @(*) begin
+    case (sel)
+        2'b00: R = A + B;   
+        2'b01: R = A & B;   
+        default: R = 5'b00000; 
+    endcase
+end
+
+endmodule
