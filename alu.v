196017a04a12023ede13e5fa59dc1497d966bafc
WorringBus <164392034+WorringBus@users.noreply.github.com>
2025-09-01 21:38:43 -0600

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,15 +1,14 @@
 module ALU (
     input  [3:0] A,     
     input  [3:0] B,     
-    input  [1:0] sel,   
-    output reg [4:0] R  
+    input  sel,   
+  output reg [3:0] R
 );
 
-always @(*) begin
+always @* begin
     case (sel)
-        2'b00: R = A + B;   
-        2'b01: R = A & B;   
-        default: R = 5'b00000; 
+        1'b0: R = A + B;   
+        1'b1: R = A & B;   
     endcase
 end
 
